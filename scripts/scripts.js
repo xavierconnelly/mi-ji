9e10c49681e014300ededf6aefbcc7889e3a639f
Xavier Connelly <44646743+xavierconnelly@users.noreply.github.com>
2021-12-19 09:27:13 +1100

    Create scripts.js

---
+++ scripts/scripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+  $(".carousel").slick({
+    dots: false,
+    fade: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    //vertical: true,
+    adaptiveHeight: true
+  });
+});
+
+function focusDetails() {
+  document.getElementById("gallery").style.opacity = "0";
+  document.querySelector("body").style.background = "rgb(255, 80, 0)";
+}
+function normalDetails() {
+  document.getElementById("gallery").style.opacity = "1";
+  document.querySelector("body").style.background = "";
+}
