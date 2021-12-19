3a2f1e2f9bf958e5ffcf28c3788f02d9da5d665e
Xavier Connelly <44646743+xavierconnelly@users.noreply.github.com>
2021-12-19 18:40:52 +1100

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,15 +1,15 @@
-$(document).ready(function () {
-  $(".carousel").slick({
-    dots: false,
-    fade: false,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    //vertical: true,
-    lazyLoad: 'progressive',
-    adaptiveHeight: true
-  });
-});
+// $(document).ready(function () {
+//   $(".carousel").slick({
+//     dots: false,
+//     fade: false,
+//     infinite: true,
+//     speed: 500,
+//     slidesToShow: 1,
+//     //vertical: true,
+//     lazyLoad: 'progressive',
+//     adaptiveHeight: true
+//   });
+// });
 
 function focusDetails() {
   document.getElementById("gallery").style.opacity = "0";
@@ -18,4 +18,92 @@ function focusDetails() {
 function normalDetails() {
   document.getElementById("gallery").style.opacity = "1";
   document.querySelector("body").style.background = "";
+}
+
+
+// Flick between images
+
+// This is the important part!
+
+function collapseSection(element) {
+  // get the height of the element's inner content, regardless of its actual size
+  var sectionHeight = element.scrollHeight;
+  
+  // temporarily disable all css transitions
+  var elementTransition = element.style.transition;
+  element.style.transition = '';
+  
+  // on the next frame (as soon as the previous style change has taken effect),
+  // explicitly set the element's height to its current pixel height, so we 
+  // aren't transitioning out of 'auto'
+  requestAnimationFrame(function() {
+    element.style.height = sectionHeight + 'px';
+    element.style.transition = elementTransition;
+    
+    // on the next frame (as soon as the previous style change has taken effect),
+    // have the element transition to height: 0
+    requestAnimationFrame(function() {
+      element.style.height = 0 + 'px';
+    });
+  });
+  
+  // mark the section as "currently collapsed"
+  element.setAttribute('data-collapsed', 'true');
+}
+
+function expandSection(element) {
+  // get the height of the element's inner content, regardless of its actual size
+  var sectionHeight = element.scrollHeight;
+  
+  // have the element transition to the height of its inner content
+  element.style.height = sectionHeight + 'px';
+
+  // when the next css transition finishes (which should be the one we just triggered)
+  element.addEventListener('transitionend', function(e) {
+    // remove this event listener so it only gets triggered once
+    element.removeEventListener('transitionend', arguments.callee);
+    
+    // remove "height" from the element's inline styles, so it can return to its initial value
+    element.style.height = null;
+  });
+  
+  // mark the section as "currently not collapsed"
+  element.setAttribute('data-collapsed', 'false');
+}
+
+
+// When called as a listener, turns the related element blue
+function bluify(e) {
+  // Always true
+  console.log(this === e.currentTarget);
+  // true when currentTarget and target are the same object
+  console.log(this === e.target);
+  //this.style.color = "#A5D9F3";
+  var plan = this.querySelector('.plan');
+  var photo = this.querySelector('.photo');
+  var isPlanCollapsed = plan.getAttribute('data-collapsed') === 'true';
+  var isPhotoCollapsed = photo.getAttribute('data-collapsed') === 'true';
+
+  if(isPlanCollapsed) {
+    expandSection(plan)
+    plan.setAttribute('data-collapsed', 'false')
+  } else {
+    collapseSection(plan)
+  }
+  
+  if(isPhotoCollapsed) {
+    expandSection(photo)
+    photo.setAttribute('data-collapsed', 'false')
+  } else {
+    collapseSection(photo)
+  }
+}
+
+// Get a list of every element in the document
+var elements = document.getElementsByClassName("container");
+
+// Add bluify as a click listener so when the
+// element is clicked on, it turns blue
+for (var i = 0; i < elements.length; i++) {
+  elements[i].addEventListener("click", bluify, false);
 }
