f214429a158728d692131989aa6dfb0e492abd4c
Xavier Connelly <44646743+xavierconnelly@users.noreply.github.com>
2022-01-26 13:37:35 +1100

    Update scripts_mobile.js

--- scripts/scripts_mobile.js
+++ scripts/scripts_mobile.js
@@ -10,20 +10,33 @@ function showFooter() {
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-// Mobile — Scrolling
+// Mobile — Project nav/slideshow
 
-const vertical = new Swiper(".vertical", {
-  // styling
-  direction: "vertical",
-  centeredSlides: false,
-  //loop
-  loop: true,
-  slidesPerView: "auto",
-  loopedSlides: 3,
-  allowSlidePrev: true, // can scroll backwards?
-  // speed
-  freeMode: true, // allow/stop scroll momentum
-  longSwipes: true // stop 
-  // setWrapperSize: true
-});
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
+}
 
